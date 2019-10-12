7bdf1eca82deb0af51c394402957d303e0d40085
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-10-13 00:29:09 +0300

    changed to JPA repository

--- src/main/java/com/apploidxxx/heliosrestapispring/entity/access/repository/queue/QueueRepository.java
+++ src/main/java/com/apploidxxx/heliosrestapispring/entity/access/repository/queue/QueueRepository.java
@@ -1,11 +1,11 @@
 package com.apploidxxx.heliosrestapispring.entity.access.repository.queue;
 
 import com.apploidxxx.heliosrestapispring.entity.queue.Queue;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author Arthur Kupriyanov
  */
-public interface QueueRepository extends CrudRepository<Queue, Long> {
+public interface QueueRepository extends JpaRepository<Queue, String> {
     Queue findByName(String name);
 }
