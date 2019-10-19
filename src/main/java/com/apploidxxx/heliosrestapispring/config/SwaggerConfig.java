653b2ba053f90a1915670cba602abd44e436a26b
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-10-19 16:08:01 +0300

    added swagger config

---
+++ src/main/java/com/apploidxxx/heliosrestapispring/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.apploidxxx.heliosrestapispring.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("net.guides.springboot2.springboot2swagger2.controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("Employee Management REST API")
+                .contact(new Contact("Arthur Kupriyanov", "vk.com/apploidxxx", "apploidyakutsk@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+}
