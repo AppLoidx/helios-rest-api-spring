ae612ce2df00cf955670749b22450f99c487aae5
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-10-19 21:05:03 +0300

    refactoring 🤩

--- src/main/java/com/apploidxxx/heliosrestapispring/api/AuthApi.java
+++ src/main/java/com/apploidxxx/heliosrestapispring/api/AuthApi.java
@@ -3,6 +3,7 @@ package com.apploidxxx.heliosrestapispring.api;
 
 import com.apploidxxx.heliosrestapispring.api.model.ErrorMessage;
 import com.apploidxxx.heliosrestapispring.api.model.Tokens;
+import com.apploidxxx.heliosrestapispring.api.util.ErrorResponseFactory;
 import com.apploidxxx.heliosrestapispring.api.util.Password;
 import com.apploidxxx.heliosrestapispring.entity.AuthorizationCode;
 import com.apploidxxx.heliosrestapispring.entity.Session;
@@ -11,7 +12,6 @@ import com.apploidxxx.heliosrestapispring.entity.access.repository.Authorization
 import com.apploidxxx.heliosrestapispring.entity.access.repository.UserRepository;
 import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -36,10 +36,21 @@ public class AuthApi {
     @ApiResponses(value = {
             @ApiResponse(
                     code = 200,
-                    message = "User authorized successfully. Client will redirect to redirect_uri or gets Tokens if redirect_uri is declared"
+                    message =   "User authorized successfully. " +
+                                "Client will redirect to redirect_uri or gets Tokens if redirect_uri is declared"
             ),
-            @ApiResponse(code = 401, message = "Invalid password or login", response = ErrorMessage.class),
-            @ApiResponse(code = 400, message = "Invalid params", response = ErrorMessage.class)
+
+            @ApiResponse(
+                    code = 401,
+                    message = "Invalid password or login",
+                    response = ErrorMessage.class
+            ),
+
+            @ApiResponse(
+                    code = 400,
+                    message = "Invalid params",
+                    response = ErrorMessage.class
+            )
     })
     @GetMapping(produces = "application/json")
     public Object authorize(
@@ -60,36 +71,47 @@ public class AuthApi {
     ) throws IOException {
 
         User user = this.userRepository.findByUsername(username);
-        if (user!=null && Password.isEqual(password, user.getPassword())) {
 
-            Session s = setUserSession(user);
-            if (redirectUri == null) {
-                response.setStatus(HttpServletResponse.SC_OK);
-                return new Tokens(s.getAccessToken(), "refresh-accessToken", user);
-            } else {
-                AuthorizationCode authorizationCode = new AuthorizationCode(user);
-                this.authorizationCodeRepository.save(authorizationCode);
-                if (state == null) state = "state";
+        if (checkAuth(user, password)) return unauthorizedErrorMessage(response);
 
-                response.sendRedirect(
-                        String.format(redirectUri + "?authorization_code=%s&state=%s", authorizationCode.getAuthCode(), state));
-                return null;
-            }
-        } else {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return new ErrorMessage("invalid_credentials", "invalid login or password");
+        Session s = setUserSession(user);
 
-        }
+        if (redirectUri == null) return generateTokens(s, response);
+
+        AuthorizationCode authorizationCode = new AuthorizationCode(user);
+        this.authorizationCodeRepository.save(authorizationCode);
+
+        response.sendRedirect(prepareRedirectUri(redirectUri, authorizationCode.getAuthCode(), state));
+        return null;
+    }
+
+    private boolean checkAuth(User user, String password){
+        return user == null || !Password.isEqual(password, user.getPassword());
+    }
+
+    private ErrorMessage unauthorizedErrorMessage(HttpServletResponse response){
+        return ErrorResponseFactory
+                .getUnauthorizedErrorResponse
+                        ("invalid_credentials", "invalid login or password", response);
     }
 
     private Session setUserSession(User user) {
 
-        Session s;
-        if (user.getSession()!=null) s = user.getSession();
-        else s = new Session();
-
+        Session s = user.getSession()!=null?user.getSession():new Session();
         s.generateSession(user);
+
         this.userRepository.save(user);
+
         return s;
     }
+
+    private Tokens generateTokens(Session session, HttpServletResponse response){
+        response.setStatus(HttpServletResponse.SC_OK);
+        return new Tokens(session.getAccessToken(), "refresh-accessToken", session.getUser());
+    }
+
+    private String prepareRedirectUri(String redirectUri, String authCode, String state){
+        return String.format(redirectUri + "?authorization_code=%s&state=%s", authCode, state==null?"state":state);
+    }
+
 }
