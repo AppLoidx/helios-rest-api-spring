4612bc7c99ac351f9e3740b4744144e8a3f5a8e7
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-10-19 21:59:13 +0300

    refactoring (and fixed error with last_msg_id param name)

--- src/main/java/com/apploidxxx/heliosrestapispring/api/ChatApi.java
+++ src/main/java/com/apploidxxx/heliosrestapispring/api/ChatApi.java
@@ -9,18 +9,18 @@ import com.apploidxxx.heliosrestapispring.entity.user.User;
 import com.apploidxxx.heliosrestapispring.entity.access.repository.SessionRepository;
 import com.apploidxxx.heliosrestapispring.entity.access.repository.queue.QueueRepository;
 import com.apploidxxx.heliosrestapispring.entity.queue.Queue;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+
 /**
  * @author Arthur Kupriyanov
  */
-@Controller
-@RequestMapping(value = "/api/chat", produces = "application/json")
+@RestController
+@RequestMapping(value = "/api/chat/{queueName}", produces = "application/json")
 public class ChatApi {
     private final QueueRepository queueRepository;
     private final SessionRepository sessionRepository;
@@ -30,11 +30,12 @@ public class ChatApi {
         this.sessionRepository = sessionRepository;
     }
 
-    @GetMapping("/{queueName}")
+    @GetMapping
     public Set<Message> getMessages(
             HttpServletResponse response,
             @PathVariable("queueName") String queueName,
-            @RequestParam("lastMsgId") int lastMsgId){
+            @RequestParam("last_msg_id") int lastMsgId
+    ){
         Queue queue = this.queueRepository.findByName(queueName);
         if (queue==null){
             return null;
@@ -50,37 +51,47 @@ public class ChatApi {
         response.setStatus(HttpServletResponse.SC_OK);
         return messageLinkedHashSet;
     }
-    @PutMapping("/{queueName}")
+
+    @PutMapping
     public ErrorMessage addMessage(
             HttpServletResponse response,
             @PathVariable("queueName") String queueName,
             @RequestParam("message") String message,
-            @RequestParam("access_token") String token){
+            @RequestParam("access_token") String token
+    ){
 
-        User user;
-        Queue q;
+        User user = this.sessionRepository.findByAccessToken(token).getUser();
+        if (user == null) return generateInvalidTokenErrorResponse(response);
 
-        if ( (user = this.sessionRepository.findByAccessToken(token).getUser()) == null){
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return new ErrorMessage("invalid_token", "Your token is invalid or expired. Please, take new token or refresh existing");
-        }
-        if ((q = this.queueRepository.findByName(queueName)) == null) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return new ErrorMessage("queue_not_found", "Queue with this name not found");
-        }
+        Queue queue = this.queueRepository.findByName(queueName);
+        if (queue  == null) return generateQueueNotFoundErrorResponse(response);
 
-        if (!q.getSuperUsers().contains(user) && !q.getMembers().contains(user)){
-            return ErrorResponseFactory.getForbiddenErrorResponse(response);
-        }
-
-        Chat chat = q.getChat();
         if ("".equals(message))
             return ErrorResponseFactory.getInvalidParamErrorResponse("Your message is invalid", response);
 
-        chat.newMessage(user, message);
-        this.queueRepository.save(q);
-        response.setStatus(HttpServletResponse.SC_OK);
-        return null;
+        if (isUserHaveAccess(queue, user)) return ErrorResponseFactory.getForbiddenErrorResponse(response);
 
+
+        Chat chat = queue.getChat();
+
+        chat.newMessage(user, message);
+        this.queueRepository.save(queue);
+        return null;
+    }
+
+    private ErrorMessage generateInvalidTokenErrorResponse(HttpServletResponse response){
+        return ErrorResponseFactory
+                .getInvalidParamErrorResponse(
+                        "invalid_token", "Your token is invalid or expired. Please, take new token or refresh existing", response);
+    }
+
+    private ErrorMessage generateQueueNotFoundErrorResponse(HttpServletResponse response){
+        return ErrorResponseFactory
+                .getInvalidParamErrorResponse(
+                        "queue_not_found", "Queue with this name not found", response);
+    }
+
+    private boolean isUserHaveAccess(Queue queue, User user){
+        return queue.getSuperUsers().contains(user) || queue.getMembers().contains(user);
     }
 }
