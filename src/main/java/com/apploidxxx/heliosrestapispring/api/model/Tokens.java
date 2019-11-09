d6172a7cb41f6a6b7d0e740f23a46046b928e2f9
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-11-09 12:30:14 +0300

    no args constructor added for use this model in unit-tests

--- src/main/java/com/apploidxxx/heliosrestapispring/api/model/Tokens.java
+++ src/main/java/com/apploidxxx/heliosrestapispring/api/model/Tokens.java
@@ -4,23 +4,29 @@ package com.apploidxxx.heliosrestapispring.api.model;
 import com.apploidxxx.heliosrestapispring.entity.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 /**
  * @author Arthur Kupriyanov
  */
+@Getter@Setter
+@NoArgsConstructor
 public class Tokens {
 
     @JsonProperty("access_token")
-    public final String token;
-    public final String refreshToken;
+    private String token;
+    private String refreshToken;
 
     @JsonIgnore
-    public final User user;
+    public User user;
 
     public Tokens(String token, String refreshToken, User user){
         this.token = token;
         this.refreshToken = refreshToken;
         this.user = user;
     }
+
 }
