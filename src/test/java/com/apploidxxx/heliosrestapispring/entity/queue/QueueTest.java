3905e36388a2c101d740c892b54301e9583e2138
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-11-15 20:32:13 +0300

    fixed JUnit test and un-ignored

--- src/test/java/com/apploidxxx/heliosrestapispring/entity/queue/QueueTest.java
+++ src/test/java/com/apploidxxx/heliosrestapispring/entity/queue/QueueTest.java
@@ -1,14 +1,13 @@
 package com.apploidxxx.heliosrestapispring.entity.queue;
 
 import com.apploidxxx.heliosrestapispring.entity.user.User;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * @author Arthur Kupriyanov
  */
 public class QueueTest {
-    @Ignore
+
     @Test
     public void test_shuffle(){
         Queue q = new Queue("123");
@@ -21,12 +20,14 @@ public class QueueTest {
         }
     }
 
-    @Ignore
+
     @Test
     public void swap_test(){
         Queue q = new Queue("123");
         User u1 = new User("111", "", "", "");
         User u2 = new User("222", "", "", "");
+        u1.setId(1L);
+        u2.setId(2L);
         q.addUser(u1);
         q.addUser(u2);
         System.out.println(q.getMembers());
