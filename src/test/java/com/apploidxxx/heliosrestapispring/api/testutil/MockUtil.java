8bd49d773e62a806dd48156c0e74fda613dd6a43
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-11-19 20:34:30 +0300

    mocked SessionRepository

--- src/test/java/com/apploidxxx/heliosrestapispring/api/testutil/MockUtil.java
+++ src/test/java/com/apploidxxx/heliosrestapispring/api/testutil/MockUtil.java
@@ -1,5 +1,7 @@
 package com.apploidxxx.heliosrestapispring.api.testutil;
 
+import com.apploidxxx.heliosrestapispring.entity.Session;
+import com.apploidxxx.heliosrestapispring.entity.access.repository.SessionRepository;
 import com.apploidxxx.heliosrestapispring.entity.access.repository.UserRepository;
 import com.apploidxxx.heliosrestapispring.entity.user.User;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,14 +12,21 @@ import static org.mockito.Mockito.when;
 /**
  *
  * Class for generate entities with mocking via {@link org.mockito.Mockito#when(Object)}
- *
- * WARING:
- *      don't forget to don't declare a such MockBean declared here
- *      because you can get an IllegalStateException, which invoked
+ * <br><br>
+ * <strong style="color: orange">WARING</strong>:
+ *      remember not to declare such MockBean declared here
+ *      because you can get an IllegalStateException that is thrown
  *      when declared two similar MockBean
  *
- * Declared MockBeans:
- *      * {@link UserRepository}
+ * <br><br>
+ * <hr>
+ * <strong>Declared MockBeans:</strong>
+ *      <ul>
+ *          <li>{@link UserRepository}</li>
+ *      </ul>
+ * <hr>
+ *
+ *     <img src="https://i.pinimg.com/564x/4b/1f/25/4b1f25db68feafeea9fa57d5da4dab6a.jpg" />
  *
  * @version 1.0.0
  * @author Arthur Kupriyanov
@@ -28,6 +37,8 @@ public class MockUtil {
     @MockBean
     private UserRepository userRepository;
 
+    @MockBean
+    private SessionRepository sessionRepository;
 
     /**
      * Create {@link User} entity via {@link UserBuilder} and
@@ -39,8 +50,9 @@ public class MockUtil {
      */
     public User getRandomUserWithMockedRepository(){
         User user = UserBuilder.createUser().build();
+        new Session().generateSession(user);
         when(this.userRepository.findByUsername(user.getUsername())).thenReturn(user);
-
+        when(this.sessionRepository.findByAccessToken(user.getSession().getAccessToken())).thenReturn(user.getSession());
         return user;
     }
 
