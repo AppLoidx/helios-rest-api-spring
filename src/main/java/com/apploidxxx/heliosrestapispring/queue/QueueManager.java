f0516e9243c527b28402419f7a330c679bb76a7a
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-12-01 15:38:42 +0300

    added move user to end of queue utility method

--- src/main/java/com/apploidxxx/heliosrestapispring/queue/QueueManager.java
+++ src/main/java/com/apploidxxx/heliosrestapispring/queue/QueueManager.java
@@ -5,11 +5,35 @@ import com.apploidxxx.heliosrestapispring.entity.user.User;
 import com.apploidxxx.heliosrestapispring.queue.controller.NextUserStrategy;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.LinkedList;
+
 /**
  * @author Arthur Kupriyanov
  */
 @Slf4j
 public class QueueManager {
+
+    /**
+     * Moves user to the end of queue
+     * <br><br>
+     *
+     * <span style="color: orange">Note</span> : this method don't modify the cursoredUsers field in {@link Queue}
+     *
+     * @param queue queue which contains this user
+     * @param user user which needed to move
+     * @return reordered Queue
+     */
+    public static Queue moveUserToEnd(Queue queue, User user)
+    {
+        LinkedList<User> linkedList = (LinkedList<User>) queue.getMembers();
+        if (linkedList.isEmpty()) return queue;
+
+        linkedList.remove(user);
+        linkedList.addLast(user);
+        queue.setMembers(linkedList);
+
+        return queue;
+    }
     public static Queue nextUser(Queue queue, User user){
         if (queue.getQueueSequence().isEmpty()) return queue;
 
