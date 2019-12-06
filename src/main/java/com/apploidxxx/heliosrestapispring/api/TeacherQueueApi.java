149f59321fb8dc8fc5fd250a2af341ad2c0a51ec
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-12-06 08:44:38 +0300

    added PostMapping

--- src/main/java/com/apploidxxx/heliosrestapispring/api/TeacherQueueApi.java
+++ src/main/java/com/apploidxxx/heliosrestapispring/api/TeacherQueueApi.java
@@ -4,6 +4,7 @@ import com.apploidxxx.heliosrestapispring.api.model.CurrentAndNextUser;
 import com.apploidxxx.heliosrestapispring.api.model.ErrorMessage;
 import com.apploidxxx.heliosrestapispring.api.util.ErrorResponseFactory;
 import com.apploidxxx.heliosrestapispring.api.util.RepositoryManager;
+import com.apploidxxx.heliosrestapispring.entity.queue.CursoredUsersWrapper;
 import com.apploidxxx.heliosrestapispring.entity.queue.Queue;
 import com.apploidxxx.heliosrestapispring.entity.user.User;
 import com.apploidxxx.heliosrestapispring.entity.user.UserType;
@@ -16,6 +17,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 
 // TODO: write test for teacher queue management [/api/teacher/queue]
@@ -47,7 +51,7 @@ public class TeacherQueueApi {
             @RequestParam("queue_name") String queueName,
             @RequestParam("access_token") String teacherAccessToken
     ){
-        User teacher = this.repositoryManager.getUser().byAccessToken(teacherAccessToken);
+        User teacher = getUserByAccessToken(teacherAccessToken);
         if (teacher.getUserType() != UserType.TEACHER) return ErrorResponseFactory.getForbiddenErrorResponse(response);
 
 
@@ -58,12 +62,23 @@ public class TeacherQueueApi {
         }
 
 
-        User currentUser = null;
-        for (User member : queue.getMembersList()){
-            if (queue.getCursoredUsers().contains(member)) continue;
 
-            currentUser = member;
-            break;
+        Set<User> currentUser = null;
+        if (queue.getCursoredUsers().containsKey(teacher) && queue.getCursoredUsers().get(teacher) != null && !queue.getCursoredUsers().get(teacher).getCursoredUsers().isEmpty()){
+            currentUser = queue.getCursoredUsers().get(teacher).getCursoredUsers();
+        }
+        else {
+
+            final Set<User> cursoredUsers = getSetOfCursoredUsers(queue);
+
+            for (User member : queue.getMembersList()) {
+                if (cursoredUsers.contains(member)) continue;
+
+                LinkedHashSet<User> set = new LinkedHashSet<>();
+                set.add(member);
+                currentUser = set;
+                break;
+            }
         }
 
         if (currentUser == null){
@@ -71,21 +86,66 @@ public class TeacherQueueApi {
             return new ErrorMessage("No users left", "All users cursored by teachers");
         }
 
-        queue.getCursoredUsers().add(currentUser);
-        CurrentAndNextUser currentAndNextUserModel  = new CurrentAndNextUser(currentUser);
+        queue.getCursoredUsers().put(teacher, new CursoredUsersWrapper(currentUser));
+        this.repositoryManager.saveQueue(queue);
+
+        CurrentAndNextUser currentAndNextUserModel  = new CurrentAndNextUser(queue.getCursoredUsers().get(teacher).getCursoredUsers());
 
 
         // TODO: refactor
         if (queue.getMembers().size() > 1){
+            final Set<User> cursoredUsers = getSetOfCursoredUsers(queue);
             for (User member : queue.getMembersList()){
-                if (!queue.getCursoredUsers().contains(member)){
+                if (!cursoredUsers.contains(member)){
                     currentAndNextUserModel.setNextUser(new CurrentAndNextUser.User(member));
                     break;
                 }
             }
         }
-        this.repositoryManager.saveQueue(queue);
+
         return currentAndNextUserModel;
+
+    }
+
+    /**
+     * Add user for cursored set (when teacher want to accept multithreaded)
+     */
+    @PostMapping
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public Object startNewUserAcceptSession(
+            HttpServletResponse response,
+            @RequestParam("username") String username,
+            @RequestParam("access_token") String teacherAccessToken,
+            @RequestParam("queue_name") String queueName
+    ){
+        User teacher = getUserByAccessToken(teacherAccessToken);
+        if (teacher.getUserType() != UserType.TEACHER) return ErrorResponseFactory.getForbiddenErrorResponse(response);
+
+
+        Queue queue = this.repositoryManager.getQueue().byQueueName(queueName);
+
+        if (queue.getMembersList().size() == 0) {
+            return ErrorResponseFactory.getInvalidParamErrorResponse("Queue don't have users yet", response);
+        }
+
+        User newUser = this.repositoryManager.getUser().byUsername(username);
+
+        if (!queue.getMembers().contains(newUser))
+            return ErrorResponseFactory.getInvalidParamErrorResponse("user not in queue", response);
+
+        Set<User> teacherUsersSet = queue.getCursoredUsers().get(teacher).getCursoredUsers();
+        if (teacherUsersSet == null) {
+            LinkedHashSet<User> linkedHashSet = new LinkedHashSet<>();
+            linkedHashSet.add(newUser);
+            queue.getCursoredUsers().put(teacher, new CursoredUsersWrapper(linkedHashSet));
+        } else {
+            Set<User> oldSet = queue.getCursoredUsers().get(teacher).getCursoredUsers();
+            oldSet.add(newUser);
+            queue.getCursoredUsers().put(teacher, new CursoredUsersWrapper(oldSet));
+        }
+        this.repositoryManager.saveQueue(queue);
+
+        return null;
 
     }
 
@@ -95,24 +155,39 @@ public class TeacherQueueApi {
     public Object userPassed(
             HttpServletResponse response,
             @RequestParam("passed_user") String passedUsername,
-            @RequestParam("access_token") String accessToken,
+            @RequestParam("access_token") String teacherAccessToken,
             @RequestParam("queue_name") String queueName
     ){
 
-        User teacher = this.repositoryManager.getUser().byAccessToken(accessToken);
+        User teacher = getUserByAccessToken(teacherAccessToken);
         if (teacher.getUserType() != UserType.TEACHER) return ErrorResponseFactory.getForbiddenErrorResponse(response);
 
 
         Queue queue = this.repositoryManager.getQueue().byQueueName(queueName);
         User passedUser = this.repositoryManager.getUser().byUsername(passedUsername);
 
-        if (!queue.getCursoredUsers().contains(passedUser)) return ErrorResponseFactory.getInvalidParamErrorResponse("User not found", response);
+        if (queue.getCursoredUsers() == null || queue.getCursoredUsers().isEmpty()){
+            return ErrorResponseFactory.getInvalidParamErrorResponse("queue is empty", response);
+        }
+        Set<User> teacherStudents = queue.getCursoredUsers().get(teacher).getCursoredUsers();
 
-        queue.getCursoredUsers().remove(passedUser);
+        if (!teacherStudents.contains(passedUser)) return ErrorResponseFactory.getInvalidParamErrorResponse("User not found", response);
+
+        teacherStudents.remove(passedUser);
         QueueManager.moveUserToEnd(queue, passedUser);
 
         this.repositoryManager.saveQueue(queue);
 
         return null;
+    }
+
+    private User getUserByAccessToken(String accessToken){
+        return this.repositoryManager.getUser().byAccessToken(accessToken);
+    }
+
+    private Set<User> getSetOfCursoredUsers(Queue queue){
+        Set<User> cursoredUsers = new HashSet<>();
+        queue.getCursoredUsers().forEach((key , elem) -> cursoredUsers.addAll(elem.getCursoredUsers()));
+        return cursoredUsers;
     }
 }
