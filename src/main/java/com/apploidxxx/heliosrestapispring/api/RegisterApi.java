6e3fa3fb2694c89732efa406160ab1e77ca69f71
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-12-18 21:55:14 +0300

    hide alert for lgtm

--- src/main/java/com/apploidxxx/heliosrestapispring/api/RegisterApi.java
+++ src/main/java/com/apploidxxx/heliosrestapispring/api/RegisterApi.java
@@ -106,7 +106,7 @@ public class RegisterApi {
         if (!usernameExist && !emailExist){
             this.userRepository.save(new User(username, Password.hash(password), firstName, lastName, email, group));
             if (RedirectUriChecker.checkIsSafe(redirectUri)){
-                response.sendRedirect(redirectUri + "?state=" + state);
+                response.sendRedirect(redirectUri + "?state=" + state);  // lgtm [java/unvalidated-url-redirection]
             } else throw getRedirectUriIsNotSafeException();
 
             return null;
